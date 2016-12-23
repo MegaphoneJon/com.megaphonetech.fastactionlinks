910a4f8b4b817066183e20e7ebddff65e3790f88
Jon goldberg <jon@palantetech.coop>
2016-12-23 03:41:38 -0500

    Improve my JS debugging statement

--- js/fal.js
+++ js/fal.js
@@ -2,7 +2,7 @@ CRM.$(function ($) {
 // Code goes here!
   $('.fast-action-link').click(function () {
     // Suppress the link-following behavior
-    alert('hello');
+    console.log($(this));
     return false;
   });
 });
