4e3128496cd8ddeae4d0b31ed15c32a06ca732d5
Jon goldberg <jon@palantetech.coop>
2016-12-23 00:04:59 -0500

    Add SQL to create/drop tables for this entity.

---
+++ sql/auto_install.sql
@@ -0,0 +1,21 @@
+-- /*******************************************************
+-- *
+-- * civicrm_fastactionlink
+-- *
+-- *******************************************************/
+CREATE TABLE `civicrm_fastactionlink` (
+
+
+     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique FastActionLink ID',
+     `uf_group_id` int unsigned NOT NULL   COMMENT 'Which profile this link is associated with.',
+     `action` varchar(64) NOT NULL   COMMENT 'e.g. addToGroup, removeFromGroup.',
+     `action_entity_id` unsigned int    COMMENT 'If the action requires an entity ID, it is stored here.',
+     `dim_on_use` tinyint   DEFAULT 1 COMMENT 'Should this link be disabled and the corresponding search result dimmed on click?',
+     `is_active` tinyint   DEFAULT 1 COMMENT 'Is this link currently active?  If not, do not show.',
+     `weight` int NOT NULL  DEFAULT 1 COMMENT 'Controls order in which links appear.' 
+,
+    PRIMARY KEY ( `id` )
+ 
+ 
+,          CONSTRAINT FK_civicrm_fastactionlink_uf_group_id FOREIGN KEY (`uf_group_id`) REFERENCES `civicrm_uf_group`(`id`) ON DELETE CASCADE  
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
