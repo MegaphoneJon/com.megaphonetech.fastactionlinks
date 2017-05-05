466ba4f663ba869ef5870598a652b615ffcd68a0
Jon goldberg <jon@palantetech.coop>
2017-05-05 15:04:40 -0400

    action_entity_id is now showing/hiding correctly, label is correct.  entityRef not built yet

--- templates/CRM/Fastactionlinks/Page/FastActionLink.js
+++ templates/CRM/Fastactionlinks/Page/FastActionLink.js
@@ -1,6 +1,29 @@
 CRM.$(function ($) {
+  displayActionEntity();
   $('#action_type').change(function () {
-    console.log($('#action_type').val());
+    displayActionEntity();
   });
 
 });
+
+/**
+ * Set up the display of the action_entity_id field.
+ * Hide if this action doesn't use it; if it does, set the label correctly,
+ *
+ * @returns {undefined}
+ */
+function displayActionEntity() {
+  action_type = CRM.$('#action_type').val();
+  params = {'action': action_type};
+  entity = CRM.api3('FastActionLink', 'getactionentity', params)
+    .done(function (result) {
+      if (result.is_error) {
+        CRM.$('#action_entity_id').parent().parent().hide();
+      } else {
+        CRM.$("label[for='action_entity_id']").html("Select a " + result.result);
+        CRM.$('#action_entity_id').parent().parent().show();
+      }
+
+    });
+
+}
